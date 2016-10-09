f5e25a0a148b5b930fc48af2ab1dac9de7ad1c5d
R.W.Majeed <raphael@majeed.de>
2016-10-09 17:31:42 +0200

    Helper method added

--- i2b2-broker/src/main/java/org/aktin/broker/i2b2/admin/DOMUtils.java
+++ i2b2-broker/src/main/java/org/aktin/broker/i2b2/admin/DOMUtils.java
@@ -1,9 +1,13 @@
 package org.aktin.broker.i2b2.admin;
 
+import java.io.ByteArrayOutputStream;
 import java.io.OutputStream;
+import java.nio.ByteBuffer;
+import java.nio.charset.Charset;
 
 import javax.xml.transform.OutputKeys;
 import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
 import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
@@ -51,4 +55,18 @@ public class DOMUtils {
 		         new StreamResult(out));
 	}
 
+	public static String toString(Node node) throws TransformerException{
+	    TransformerFactory tf = TransformerFactory.newInstance();
+	    Transformer transformer;
+			transformer = tf.newTransformer();
+	    transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "yes");
+	    transformer.setOutputProperty(OutputKeys.METHOD, "xml");
+	    transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+	    transformer.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
+	    transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "4");
+	    ByteArrayOutputStream out = new ByteArrayOutputStream();
+	    transformer.transform(new DOMSource(node), 
+	         new StreamResult(out));
+	    return Charset.forName("UTF-8").decode(ByteBuffer.wrap(out.toByteArray())).toString();
+	}
 }
