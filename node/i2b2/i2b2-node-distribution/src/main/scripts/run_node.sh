75da4775560076bd39897a58cf8425382d941c19
R.W.Majeed <raphael@majeed.de>
2017-02-09 21:26:00 +0100

    Default API keys changed to match shrine with node distributions

--- node/i2b2/i2b2-node-distribution/src/main/scripts/run_node.sh
+++ node/i2b2/i2b2-node-distribution/src/main/scripts/run_node.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-java -cp lib/\* de.li2b2.shrine.node.i2b2.I2b2Node http://localhost:8080/broker/ asdf123 'http://services.i2b2.org/i2b2/services/PMService/|https://www.i2b2.org/webclient/index.php' demo@i2b2demo demouser i2b2_to_i2b2.xsl
+java -cp lib/\* de.li2b2.shrine.node.i2b2.I2b2Node http://localhost:8080/broker/ xxxApiKey567 'http://services.i2b2.org/i2b2/services/PMService/|https://www.i2b2.org/webclient/index.php' demo@i2b2demo demouser i2b2_to_i2b2.xsl
  
 # for testing, you can use the random count node
-#java -cp lib/\* de.li2b2.shrine.node.i2b2.RandomCountNode http://localhost:8080/broker/ asdf123 
+#java -cp lib/\* de.li2b2.shrine.node.i2b2.RandomCountNode http://localhost:8080/broker/ xxxApiKey567 
